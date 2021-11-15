0dae8a2eaf0ec4b0213e7c2e32c5c5e84cee21a5
Manfred Hästmark <hastmark@student.chalmers.se>
2021-11-15 17:03:10 +0100

    Added project structure

---
+++ src/src/main/java/backend/Main.java
@@ -0,0 +1,12 @@
+package backend;
+
+public class Main {
+
+    public static void main(String[] args){
+        new Main();
+    }
+
+    public Main() {
+        System.out.println("Hello World!");
+    }
+}
